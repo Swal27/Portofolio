428db73815d07dd039f79e6b2183bf604f71ed88
Swal27 <87480134+Swal27@users.noreply.github.com>
2024-12-10 16:37:18 +0700

    <Init> i'm not happy

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { LandingPages } from './components/pages/landingpage'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col'>
+        <LandingPages />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/landingpage/index.tsx
@@ -0,0 +1,9 @@
+
+
+export function LandingPages(){
+    return(
+        <div>
+            <h1>Hello world</h1>
+        </div>  
+    );
+}
